66a1026d7a5d63bb28600aca21425f257ee3a43b
ehdgks0627 <ehdgks0627@naver.com>
2017-08-28 22:21:55 +0900

    뼈대 소스 생성

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import RPi.GPIO as gpio
+import requests
+import json
 
-gpio.setmode(gpio.BCM)
 
-#init pins
-gpio.setup(27, gipo.OUT)
-gpio.setup(28, gipo.IN)
+class LightManager:
+    HOST = "http://a.com"
+    deviceID = "1234"
+    itemPK = -1
+    lightColor = (128, 64, 200)
+    lightStrength = 50
+    PIN_W = 28
+    PIN_
 
-try:
-    pass
-except KeyboardInterrupt:
-    gpio.cleanup()
+    def updateState(self):
+        data = {"deviceID": self.deviceID}
+        response = requests.pos(HOST, data=data)
+        if response.status_code != 200:
+            print("[-] ServerError...")
+        else:
+            body = json.loads(response.text)
+            if body["state"] == "OK":
+                #TODO
+                pass
+            else:
+                print("[-] FormatError...")
+
+    def getW(self):
+        #TODO
+        pass
+
+    def __init__(self, mode=gpio.BCM):
+        gpio.setmode(mode)
+        gpio.setup(pin_W, gipo.IN)
+
+    def __str__(self):
+        return "LightManager@%s"%(deviceID)
+
+    def __del__(self):
+        gpio.cleanup()
+
+
+def main():
+    try:
+        manager = LightManager()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()

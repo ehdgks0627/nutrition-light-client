608df03addb491669d0e6b9a8396a8f830e00afb
sprout <sprout@sprouts-MacBook-Pro.local>
2017-09-03 08:32:56 +0900

    컨트롤 동작 추가:

--- main.py
+++ main.py
@@ -9,29 +9,42 @@ class LightManager:
     itemPK = -1
     lightColor = (128, 64, 200)
     lightStrength = 50
-    PIN_W = 28
-    PIN_
+    PIN = {"consumption": 28, "temperature": 29}
 
     def updateState(self):
-        data = {"deviceID": self.deviceID}
-        response = requests.pos(HOST, data=data)
+        data = {"deviceID": self.deviceID, "consumption": readSensor("consumption"), "temperature": readSensor("temperature")}
+        response = requests.pos(HOST + "/StateDevice", data=data)
         if response.status_code != 200:
             print("[-] ServerError...")
         else:
             body = json.loads(response.text)
             if body["state"] == "OK":
                 #TODO
+                #controlDevice(body["item"])
                 pass
             else:
                 print("[-] FormatError...")
 
-    def getW(self):
+    def readSensor(self, command):
+        if command not in PIN:
+            print("%s is not in PIN MAP"%(command))
+            return
+        if command == "consumption":
+            pass
+        elif command == "temperature":
+            pass
         #TODO
+        pass
+
+    def controlDevice(self, item):
+        item["amount"]
+        item["temperature"]
+        item["time"]
         pass
 
     def __init__(self, mode=gpio.BCM):
         gpio.setmode(mode)
-        gpio.setup(pin_W, gipo.IN)
+        gpio.setup(PIN["W"], gipo.IN)
 
     def __str__(self):
         return "LightManager@%s"%(deviceID)
